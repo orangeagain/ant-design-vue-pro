fcd333b067d55bb898fff84b12ffbfe4da201d65
Your Name <orangetech@yeah.net>
2022-11-05 21:19:16 +0800

    feat:  整理成两个页面

--- src/views/profile/basic/index.vue
+++ src/views/profile/basic/index.vue
@@ -7,17 +7,16 @@ export default {
   components: {},
   mounted: function () {
     console.log('mounted')
+    // 给父DOM Main设定总体高度
     var m = document.getElementsByTagName('main')[0]
     m.setAttribute('class', 'fkmain')
     console.log(m)
+    // 子组件全部遵从父组件高度
     var nodes = m.childNodes
     nodes[0].setAttribute('class', 'fullh')
     var ides = nodes[0].childNodes
     ides[0].setAttribute('class', 'fullh')
     console.log('ides[0]:' + ides[0])
-    // var ide2 = ides[0].getElementsByTagName('div')[0]
-    // console.log(ide2)
-    // ide2[0].setAttribute('class', 'fkmain')
     // 渲染graphiql
     window.ReactDOM.render(
       window.React.createElement(window.GraphiQL, {
@@ -52,10 +51,12 @@ export default {
   width: 100%;
   background-color: #abbaea;
 }
+
 .fullh {
   height: 100%;
   background-color: #003afa;
 }
+
 #graphiql {
   height: 100%;
 }
